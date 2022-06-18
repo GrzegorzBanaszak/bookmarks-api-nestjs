10a8dd0635cabb7e5045ca081ab595cb25f9be34
GrzegorzBanaszak <grzegorz.banaszak95@gmail.com>
2022-06-18 14:05:14 +0200

    Add singup and singin

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,18 @@
-import { Controller, Post } from "@nestjs/common";
+import { AuthDto } from "./dto";
+import { Body, Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 
 @Controller("auth")
-
 export class AuthController {
-    constructor(private authService: AuthService) { }
-    @Post("singup")
-    singup() {
-        return this.authService.singup()
-    }
-    @Post("singin")
-    singin() {
-        return this.authService.singin()
-    }
+  constructor(private authService: AuthService) {}
+
+  @Post("singup")
+  singup(@Body() dto: AuthDto) {
+    console.log(dto);
+    return this.authService.singup(dto);
+  }
+  @Post("singin")
+  singin(@Body() dto: AuthDto) {
+    return this.authService.singin(dto);
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,49 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaService } from "../prisma/prisma.service";
-
+import { AuthDto } from "./dto";
+import * as argon2 from "argon2";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 @Injectable()
 export class AuthService {
-    constructor(private prisma: PrismaService) { }
+  constructor(private prisma: PrismaService) {}
 
-    singin() {
-        return { msg: "i am singin" };
+  async singup(dto: AuthDto) {
+    //Generate hash password
+    const hash = await argon2.hash(dto.password);
+    //Save the new user
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash: hash,
+        },
+      });
+      delete user.hash;
+
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === "P2002") {
+          throw new ForbiddenException("Credentials taken");
+        }
+      }
+      throw error;
     }
-    singup() {
-        return { msg: "i am singup" };
-    }
+  }
+  async singin(dto: AuthDto) {
+    //Find the user be email
+
+    const user = await this.prisma.user.findUnique({ where: { email: dto.email } });
+    //If user not found throw error
+
+    if (!user) throw new ForbiddenException("Credentials invalid");
+
+    const pwMatches = await argon2.verify(user.hash, dto.password);
+
+    if (!pwMatches) throw new ForbiddenException("Credentials invalid");
+
+    delete user.hash;
+
+    return user;
+  }
 }
